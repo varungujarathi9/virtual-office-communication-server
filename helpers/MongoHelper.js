89121789f9bd09c181ac5627f658dd7aa85fb88a
Varun Gujarathi <v.gujarathi777@gmail.com>
2021-02-23 19:15:45 +0530

    added basic module

---
+++ helpers/MongoHelper.js
@@ -0,0 +1,17 @@
+use strict;
+...
+// import modules
+var MongoClient = require('mongodb').MongoClient;
+var configs = require("../configs.json");
+
+// declare and initiate variables
+var mongoHost = configs["mongo-db-host"];
+
+insert = (jsonVar) =>{
+    use strict MongoClient.connect(mongoHost, (err, db) =>{
+        if (err) throw err;
+
+        var dbo = db.db(configs["mongo-db-name"]);
+
+    })
+}
---
+++ index.js
@@ -0,0 +1,16 @@
+use strict
+import express from 'express'
+import { json } from 'express'
+
+const app = express()
+const port = 8000
+
+app.use(json())
+
+app.get('/',(req,res)=>{
+  res.send("Virtual Office Server")
+})
+
+app.post('/register',(req,res)=>{
+  console.log(req.body)
+})
