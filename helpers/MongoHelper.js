973b4ce567723ef60f02c970e85e005d68b2ea15
Varun Gujarathi <v.gujarathi777@gmail.com>
2021-02-28 13:33:17 +0530

    reqister endpoint username validation done

--- helpers/MongoHelper.js
+++ helpers/MongoHelper.js
@@ -1,16 +1,63 @@
 // "use strict";
 // import modules
-var MongoClient = require('mongodb').MongoClient;
-var configs = require("../configs.json");
+var MongoClient = require('mongodb').MongoClient
+var configs = require("../configs.json")
 
 // declare and initiate variables
-var mongoHost = configs["mongo-db-host"];
+var mongoHost = configs["mongo-db-host"]
+var mongoDbName = configs["mongo-db-name"]
 
-insert = (jsonVar) =>{
-    MongoClient.connect(mongoHost, (err, db) => {
-        if (err) throw err;
+module.exports = {
+    insertOne: function(jsonData, tableName){
+        MongoClient.connect(mongoHost, (err, db) => {
+            if (err) throw err;
 
-        var dbo = db.db(configs["mongo-db-name"]);
+            var dbo = db.db(mongoDbName);
+            dbo.collection(tableName).insertOne(jsonData, (err, res) => {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+            })
+        })
+    },
+    insertMany: function(jsonData, tableName){
+        MongoClient.connect(mongoHost, (err, db) => {
+            if (err) throw err;
 
-    })
+            var dbo = db.db(mongoDbName);
+            dbo.collection(tableName).insertMany(jsonData, (err, res) => {
+                if (err) throw err;
+                console.log("documents inserted");
+                db.close();
+            })
+        })
+    },
+    find: function(key, value, tableName){
+        MongoClient.connect(mongoHost, (err, db) => {
+            if (err) throw err;
+
+            var dbo = db.db(mongoDbName);
+            dbo.collection(tableName).find({key:value}).toArray((err, res) => {
+                if (err) throw err;
+                console.log(result);
+                db.close();
+                return result
+            })
+        })
+    },
+    exists: function(key, value, tableName){
+        MongoClient.connect(mongoHost, (err, db) => {
+            if (err) throw err;
+
+            var dbo = db.db(mongoDbName);
+            var exists_count = dbo.collection(tableName).find({key:value}).count()
+            if (exists_count > 0){
+                return true
+            }
+            else{
+                return false
+            }
+
+        })
+    }
 }
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // "use strict";
+const mongoHelper = require("./helpers/MongoHelper")
 const express = require('express')
+const cors = require('cors')
+const configs = require("./configs.json")
+
 const app = express()
 const port = process.env.PORT || 8000
+const userCollection = configs["mongo-user-collection"]
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/',(req,res)=>{
@@ -10,8 +16,46 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/register',(req,res)=>{
+  console.log("Request: " + JSON.stringify(req.body))
+  registrationResponse = {}
+  // checking if the request body has required fields
+  if(req.body.hasOwnProperty('firstname') && req.body.hasOwnProperty('lastname') && req.body.hasOwnProperty('username') && req.body.hasOwnProperty('email') && req.body.hasOwnProperty('password')){
+
+    // check if username is already taken
+    try{
+      if (mongoHelper.exists("username", req.body["username"], "users") == false){
+        try{
+          mongoHelper.insert(req.body, userCollection)
+          registrationResponse["registration-status"] = "successful"
+        }
+        catch(err){
+          console.log(err)
+          registrationResponse["registration-status"] = "unsuccessful"
+          registrationResponse["reason"] = err
+        }
+      }
+      else{
+        registrationResponse["registration-status"] = "unsuccessful"
+        registrationResponse["reason"] = "username already exists"
+      }
+    }
+    catch{
+      console.log(err)
+      registrationResponse["registration-status"] = "unsuccessful"
+      registrationResponse["reason"] = err
+    }
+  }
+  else{
+    registrationResponse["registration-status"] = "unsuccessful"
+    registrationResponse["reason"] = "incorrect form data"
+  }
+  console.log("Response: " + JSON.stringify(registrationResponse))
+  res.send(registrationResponse)
+})
+
+app.post('/login',(req,res)=>{
   console.log(req.body)
-  res.send({"registration-status":"successful"})
+  res.send({"login-status":"successful"})
 })
 
 app.listen(port, () => {
